1d9152d6813d38aefca9e1faef85a6bcb9402629
Alexandre Magno <alexandre.mbm@gmail.com>
2014-09-03 18:25:02 -0300

    Closes #1: LICENSE into files

--- GM_osm-forum_WYSIWYG-editor.user.js
+++ GM_osm-forum_WYSIWYG-editor.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        OpenStreetMap Forum's WYSIWYG editor
-// @namespace   GM_osm-forum-WYSIWYG
+// @namespace   GM_osm-forum
 // @description BBCode editor for the OpenStreetMap Forum
 // @include     http://forum.openstreetmap.org/viewtopic.php?id=*
 // @include     http://forum.openstreetmap.org/viewtopic.php?pid=*
@@ -12,6 +12,28 @@
 // @require     http://cdn.wysibb.com/js/jquery.wysibb.min.js
 // @require     http://www.wysibb.com/js/custom.js
 // ==/UserScript==
+
+/*
+  Copyright (c) 2014 Alexandre Magno <alexandre.mbm@gmail.com>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+  SOFTWARE.
+*/
 
 /*$("div.infldset.txtarea").attr({
   style: 'display:none'
